bab6d3c8ee1a77b3cbff810e5f0bddcc81810fef
austinchan3678 <147582674+austinchan3678@users.noreply.github.com>
2024-04-27 08:57:04 +0000

    asasd

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,17 +163,9 @@ with c2:
 
                         with col2:
                             st.header("  " + restaurantList[i].name, divider='gray')
-                            pc1, pc2, pc3, pc4 = st.columns(4)
-                            with pc1:
-                                st.write(str(restaurantList[i].rating) + ' :star:')
-                            with pc2:
-                                st.write(restaurantList[i].review_count)
-                            with pc3:
-                                dollars = str(restaurantList[i].price)
-                                st.markdown(f':green[{dollars}]')
-                            with pc4:
-                                st.write(restaurantList[i].phone)
-                            st.subheader(restaurantList[i].address)
+
+                            st.write(str(restaurantList[i].rating) + ' :star:' + ' (' + restaurantList[i].review_count + 'reviews)' + '   |   ' + str(restaurantList[i].price))
+                            st.write(restaurantList[i].address + "  |  :telephone_receiver:" + restaurantList[i].phone)
                             mc1, mc2 = st.columns(2)
                             mapsaddr = restaurantList[i].name + "+" + restaurantList[i].address
                             mapsaddr = mapsaddr.replace(" ", "+")
