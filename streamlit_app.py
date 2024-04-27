e56ca542b7f82c9ec6b018058675c9baf719a98e
austinchan3678 <147582674+austinchan3678@users.noreply.github.com>
2024-04-27 05:25:41 +0000

    Again

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def callback():
 
 c1, c2, c3 = st.columns([1, 4, 1], gap="small")
 with c2:
-    st.image('yumsum.png', width=400)
+    st.image('yumsum(3).png')
 
     header = st.container()
     cuisine, location, results, cost = st.columns(4)
