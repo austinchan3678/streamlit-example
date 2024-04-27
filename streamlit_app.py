2f0fbbd232b8fd1433b7f4def111b5f51a08e620
austinchan3678 <147582674+austinchan3678@users.noreply.github.com>
2024-04-27 09:18:53 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,8 +163,9 @@ with c2:
 
                         with col2:
                             st.header("  " + restaurantList[i].name, divider='gray')
-
-                            st.write(str(restaurantList[i].rating) + ' :star:   (' + str(restaurantList[i].review_count) + ' reviews)' + ',   ' + str(restaurantList[i].price))
+                            st.text(str(restaurantList[i].rating) + ' :star:   (' + str(restaurantList[i].review_count) + ' reviews)' + '$    -    $' + str(restaurantList[i].price))
+                            #st.write(str(restaurantList[i].rating) + ' :star:   (' + str(restaurantList[i].review_count) + ' reviews)' + '$    -    $' + str(restaurantList[i].price))
+                            st.text(':telephone_receiver:: ' + restaurantList[i].phone + '    -    ' + restaurantList[i].address)
                             st.write(':telephone_receiver:: ' + restaurantList[i].phone + '    -    ' + restaurantList[i].address)
                             mc1, mc2 = st.columns(2)
                             mapsaddr = restaurantList[i].name + "+" + restaurantList[i].address
